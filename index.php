dbaf82fe56750eb24ead362e8a53f361da85433b
jputanier <j.putanier@gmail.com>
2019-05-13 16:05:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Voyage Voyages</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div>
+        <h1>Voyage Voyages</h1>
+    </div>
+    <nav>
+        <p>Menu</p>
+    </nav>
+    <main>
+    <div id="bloc_page">
+        <div id="bloc1">aaa</div>
+        <div id="bloc2">bbb</div>
+        <div id="bloc3">ccc</div>
+    </div>
+    </main>
+</body>
+</html>
