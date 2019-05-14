58c3839c17d1815fef740053fb5d0ac1d6fa2d6c
jputanier <j.putanier@gmail.com>
2019-05-14 13:36:53 +0200

    second commit

--- index.php
+++ index.php
@@ -5,20 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Voyage Voyages</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <div>
         <h1>Voyage Voyages</h1>
+        <div id="div_logo_user"><a href="/">
+            <figure>
+                <i class="fas fa-user logo_user"></i>
+                <figcaption>Connection</figcaption>
+            </figure></a>
+        </div>
+        <div id="div_logo_user"><a href="/">
+            <figure>
+                <i class="fas fa-user-times logo_user"></i>
+                <figcaption>Déconnection</figcaption>
+            </figure></a>
+        </div>
+        <div><a href="/"><img src="./img/ukflag.png" alt="ukflag" class="ukflag"></a></div>
+        <div><a href="/"><img src="./img/frflag.png" alt="frflag" class="frflag"></a></div>
     </div>
     <nav>
         <p>Menu</p>
     </nav>
     <main>
     <div id="bloc_page">
-        <div id="bloc1">aaa</div>
-        <div id="bloc2">bbb</div>
-        <div id="bloc3">ccc</div>
+        <div id="bloc1" class="font_kingthings"><a href="/" class="titles">New York<br/>City</a></div>
+        <div id="bloc2" class="font_america"><a href="/" class="titles">* Beyrouth *</a></div>
+        <div id="bloc3" class="font_desyrel"><a href="/" class="titles">San Francisco</a></div>
     </div>
     </main>
 </body>
