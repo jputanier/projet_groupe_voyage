ec11962043a7ef37a7e6b9c218334a9f1966d7f7
rafah77 <raphaelarronisengie@gmail.com>
2019-05-14 16:12:14 +0200

    epouie

--- index.php
+++ index.php
@@ -4,30 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
-    <title>Voyage Voyages</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
-=======
     <!-- font awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <!-- bootstrap -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    <link rel="stylesheet" href="./css/style.css">
     <title>Voyage VoyagJ</title>
->>>>>>> 5dc5835492c54876be651ff0178ec9e75f54d132
 </head>
 <body>
     <!-- header -->
     <div>
-<<<<<<< HEAD
-        <h1>Voyage Voyages</h1>
-        <div id="div_logo_user">
-            <figure>
-                <i class="fas fa-user logo_user"></i>
-                <figcaption>Connection</figcaption>
-            </figure>
-        </div>
-=======
         <h1>Voyage Voaijay</h1>
         <div id="div_logo_user"><a href="/">
             <figure>
@@ -43,25 +28,16 @@
         </div>
         <div><a href="/"><img src="./img/ukflag.png" alt="ukflag" class="ukflag"></a></div>
         <div><a href="/"><img src="./img/frflag.png" alt="frflag" class="frflag"></a></div>
->>>>>>> 5dc5835492c54876be651ff0178ec9e75f54d132
     </div>
     <!--nav-bar-->
     <?php require "./templates/header.php"?>
     <!--main-->
     <main>
-<<<<<<< HEAD
-    <div id="bloc_page">
-        <div id="bloc1" class="font_kingthings"><a href="/" class="titles">New York<br/>City</a></div>
-        <div id="bloc2" class="font_america"><a href="/" class="titles">* Beyrouth *</a></div>
-        <div id="bloc3" class="font_desyrel"><a href="/" class="titles">San Francisco</a></div>
-    </div>
-=======
         <div id="bloc_page">
             <div id="bloc1" class="font_kingthings"><a href="/" class="titles">New York<br/>City</a></div>
             <div id="bloc2" class="font_america"><a href="/" class="titles">* Beyrouth *</a></div>
             <div id="bloc3" class="font_desyrel"><a href="/" class="titles">San Francisco</a></div>
         </div>
->>>>>>> 5dc5835492c54876be651ff0178ec9e75f54d132
     </main>
     <!--footer-->
     <?php require "./templates/footer.html"?>
