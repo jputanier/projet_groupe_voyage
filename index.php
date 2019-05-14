4a94f263d3f46a0105ffadf24c2ac13e1d91aa69
L2s69 <saoucilakdar@hotmail.com>
2019-05-14 14:48:52 +0200

    ajout new

--- index.php
+++ index.php
@@ -5,12 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    <link rel="stylesheet" href="./css/style.css">
-    <title>Voyage Voyages</title>
+    <title>Voyage VoyagJ</title>
 </head>
 <body>
+    <!--nav-bar-->
     <?php require "./templates/header.php"?>
-    
-
+    <!--main-->
+    <div class="container text-center marge d-flex justify-content-center">
+        <div class=" d-flex justify-content-center">
+            <div class="col">
+                <a href="/"><img src="./img/USA.jpg" class="img-rounded" alt="Golden Gate"></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="./img/beyrouth.jpg" class="img-rounded" alt="Golden Gate"></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="./img/GoldGate.jpeg" class="img-rounded" alt="Golden Gate"></a>
+            </div>
+        </div>
+    </div>
+    <!--footer-->
+    <?php require "./templates/footer.html"?>
+    <!-- jquery popper bootstrap -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
