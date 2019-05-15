2c5c8769c79aac9dc56789c4a1f8cffbb72f9d82
L2s69 <saoucilakdar@hotmail.com>
2019-05-15 12:37:55 +0200

    modif

--- concept.php
+++ concept.php
@@ -8,8 +8,9 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <!-- bootstrap -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">   
+    <!-- css -->
     <link rel="stylesheet" href="./css/style.css">
-    <title>VOAYAJ, voyager autrement</title>
+    <title>Notre concept</title>
 </head>
 <body>
     <!-- header -->
@@ -20,11 +21,14 @@
     <?php require "./templates/header.html"?>
     <!-- main -->
     <main>
-        <div id="bloc_page">
-            <div id="bloc1" class="font_kingthings"><a href="/" class="titles">New York<br/>City</a></div>
-            <div id="bloc2" class="font_america"><a href="/" class="titles">* Beyrouth *</a></div>
-            <div id="bloc3" class="font_desyrel"><a href="/" class="titles">San Francisco</a></div>
+    <div class=row>
+        <div class=col-xs-5 col-md-3 col-lg-1 centreVerticalement>
+            <div style=height:10em;border:1px solid red>Big</div>
+            </div><!---->
+            <div class=col-xs-5 col-md-7 col-lg-9 centreVerticalement>
+            <div style=height:3em;border:1px solid #F00>Small</div>
         </div>
+    </div>
     </main>
     <!-- footer -->
     <?php require "./templates/footer.html"?>
